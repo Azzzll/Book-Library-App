01c5b31536c42da2d2f94a90d6ed15e678cc8a91
Azzzll <coolpotcanchik@gmail.com>
2025-08-01 18:56:40 +0400

    Sync changes

--- frontend/src/compontnts/BookList/BookList.js
+++ frontend/src/compontnts/BookList/BookList.js
@@ -4,6 +4,10 @@ import "./BookList.css";
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const handleDeleteBook = (id) => {
+    console.log(id);
+  };
+
   return (
     <div className='app-block book-list'>
       <h2>book list</h2>
@@ -16,6 +20,11 @@ const BookList = () => {
             <li key={book.id}>
               <div className='book-info'>
                 {++i}. {book.title} by <strong>{book.author}</strong>
+              </div>
+              <div className='book-actions'>
+                <button onClick={() => handleDeleteBook(book.id)}>
+                  Delete
+                </button>
               </div>
             </li>
           ))}

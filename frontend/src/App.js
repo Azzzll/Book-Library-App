ea033e1bfc86f58060fb33840ca20a15be8fdad1
Azzzll <coolpotcanchik@gmail.com>
2025-08-03 22:07:09 +0400

    Test sync

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import BookForm from './compontnts/BookForm/BookForm';
 import Filter from './compontnts/Filter/Filter';
 import './App.css';
 
-
 function App() {
   return (
     <div className="app">

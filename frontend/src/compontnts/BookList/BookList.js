79ce8944f5989ac51056f4de88b594320dcc0130
Azzzll <coolpotcanchik@gmail.com>
2025-08-06 01:01:08 +0400

    Add book source

--- frontend/src/compontnts/BookList/BookList.js
+++ frontend/src/compontnts/BookList/BookList.js
@@ -67,7 +67,8 @@ const BookList = () => {
             <li key={book.id}>
               <div className="book-info">
                 {++i}. {highLightMatch(book.title, titleFilter)} by{' '}
-                <strong>{highLightMatch(book.author, authorFilter)}</strong>
+                <strong>{highLightMatch(book.author, authorFilter)} </strong>(
+                {book.source})
               </div>
               <div className="book-actions">
                 <span onClick={() => handleToggleFavorite(book.id)}>
